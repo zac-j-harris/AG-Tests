54b315bc0a4483d169fe9de393d36c487f67eae8
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2021-11-19 17:28:10 -0700

    Update cnn.py

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,8 +1,5 @@
-import tensorflow as tf
 import numpy as np
-import os, csv, pickle, lzma
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+import os, csv, pickle, lzma, math
 
 data_fname = '~/Datasets/mnist'
 datasets = ['mnist_train', 'mnist_test']
@@ -40,6 +37,7 @@ def convert_csv(compressed=False):
 
 
 def gather_data(scale_subset=1):
+	import tensorflow as tf
 	x = [None, None]
 	y = [None, None]
 	for i in range(2):
@@ -64,24 +62,18 @@ def gather_data(scale_subset=1):
 
 
 def build_model():
+	from tensorflow.keras import Sequential
+	from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 	'''
 	https://www.analyticsvidhya.com/blog/2020/02/learn-image-classification-cnn-convolutional-neural-networks-3-datasets/
 
 	'''
-	# building a linear stack of layers with the sequential model
 	model = Sequential()
-	# convolutional layer
-	model.add(
-		Conv2D(25, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(28, 28, 1)))
+	model.add(Conv2D(25, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(28, 28, 1)))
 	model.add(MaxPool2D(pool_size=(1, 1)))
-	# flatten output of conv
 	model.add(Flatten())
-	# hidden layer
 	model.add(Dense(100, activation='relu'))
-	# output layer
 	model.add(Dense(10, activation='softmax'))
-
-	# compiling the sequential model
 	model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 	return model
 
@@ -101,6 +93,8 @@ def add_temporal_data(x, y):
 
 
 def main():
+	import tensorflow as tf
+
 	# x, y = gather_data(scale_subset=1000)
 	# x_train, x_test = x[0], x[1]
 	# y_train, y_test = y[0], y[1]
@@ -118,13 +112,125 @@ def main():
 	model = build_model()
 
 	# training the model for 10 epochs
-	model.fit(x_train, y_train, batch_size=128, epochs=10, validation_data=(x_test, y_test))
+	model.fit(x_train, y_train, batch_size=128, epochs=12, validation_data=(x_test, y_test))
 	# model.fit(x_train, y_train, batch_size=2, epochs=10, validation_data=(x_test, y_test))
 
 	model.save('cnn_model')
 
-	# model = tensorflow.keras.models.load_model('path/to/location')
+
+
+def color_corrected():
+	from PIL import Image
+	for i in range(1, 281):
+	# for i in range(1, 31):
+		rgb = np.asarray(Image.open("CNN_Data/split_data/" + str(i) + ".png")).copy()
+		alpha = np.asarray([[[0 if min(col) > 170 else 255 for _ in col] for col in row] for row in rgb[:,:,:3]], dtype=np.uint8)
+		img = Image.fromarray(alpha, 'RGB')
+		img.show()
+		# quit()
+		img.save("CNN_Data/color_corrected/" + str(i) + ".png")
+		quit()
+
+
+def cropped():
+	from PIL import Image
+	for i in range(1, 281):
+	# for i in range(69, 90):
+		rgb = np.asarray(Image.open("CNN_Data/color_corrected/" + str(i) + ".png")).copy()
+		bounds = [-1, -1, -1, -1]# left, right, up, down
+		rows = [sum(row) for row in rgb[:,:,0]]
+		# print(rows)
+		for j in range(len(rows)):
+			if rows[j] >=3 and bounds[2] == -1:
+				bounds[2] = j
+
+			if rows[j] >= 3:
+				bounds[3] = j
+		bounds[2] = max(bounds[2]-1, 0)
+		bounds[3] = min(bounds[3]+1, len(rows)-1)
+
+		cols = [sum(rgb[:,j,0]) for j in range(len(rgb[0]))]
+		for j in range(len(cols)):
+			if cols[j] >=3 and bounds[0] == -1:
+				bounds[0] = j
+
+			if cols[j] >= 3:
+				bounds[1] = j
+		bounds[0] = max(bounds[0]-1, 0)
+		bounds[1] = min(bounds[1]+1, len(cols)-1)
+		old_shape = rgb.shape
+		rgb = rgb[bounds[2]:bounds[3], bounds[0]:bounds[1], :]
+		# print(rgb.shape, old_shape, bounds)
+		# if min(rgb.shape) < 1:
+		# 	print(rows)
+		img = Image.fromarray(rgb, 'RGB')
+		# img.show()
+
+		img.save("CNN_Data/cropped/" + str(i) + ".png")
+
+
+
+def scaled():
+	from PIL import Image
+	for f_num in range(1, 281):
+	# for f_num in range(1, 31):
+		rgb = np.asarray(Image.open("CNN_Data/cropped/" + str(f_num) + ".png")).copy()
+		new_dim = max(rgb.shape)
+		if new_dim == rgb.shape[1]:
+			old_dim = rgb.shape[0]
+			half = (new_dim - old_dim) // 2
+			rgb = np.asarray([[ rgb[row - half, col, :] if row >= half and (row - half < old_dim) else [0, 0, 0]
+			                         for col in range(new_dim)] for row in range(new_dim)], dtype=np.uint8)
+		else:
+			old_dim = rgb.shape[1]
+			half = (new_dim - old_dim) // 2
+			rgb = np.asarray([[rgb[row, col-half, :] if col >= half and (col - half < old_dim) else [0, 0, 0]
+			                   for col in range(new_dim)] for row in range(new_dim)], dtype=np.uint8)
+		img = Image.fromarray(rgb, 'RGB')
+		img = img.resize((28, 28), Image.ANTIALIAS)
+		# img.show()
+		# quit()
+		img.save("CNN_Data/final_scaled/" + str(f_num) + ".png")
+
+
+def get_labels():
+	import csv
+	import tensorflow as tf
+	with open('CNN_Data/Labels.csv', 'r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
+		out = [i for i in reader]
+	out = out[1:]
+	out = [int(i[1]) for i in out]
+	return np.asarray(tf.keras.utils.to_categorical(out, 10))
+
+
+def test_model():
+	import tensorflow as tf
+	from PIL import Image
+	model = tf.keras.models.load_model('cnn_model')
+	x = np.asarray([np.asarray(Image.open("CNN_Data/final_scaled/" + str(f_num) + ".png")).copy()[:,:,0]/255 for f_num in range(1, 281)])
+	x = np.reshape(x, (280, 28, 28, 1))
+	pred_labels = model.predict(x)
+	labels = get_labels()
+	mse = lambda a, b: (np.square(np.subtract(a, b))).mean()
+	print("MSE = ", mse(labels, pred_labels))
+	def indexOfMax(l):
+		m = max(l)
+		for i in range(len(l)):
+			if l[i] == m:
+				return i
+	predictions = [indexOfMax(i) for i in pred_labels]
+	original = [indexOfMax(i) for i in labels]
+	num_matching = 0
+	for i in range(len(original)):
+		if original[i] == predictions[i]:
+			num_matching += 1
+	print("Accuracy = ", num_matching / len(original) * 100.0)
+
 
 
 if __name__=='__main__':
 	main()
+	# test_model()
+	# scaled()
+	# test_model()

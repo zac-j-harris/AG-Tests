56d749b316a35de7082281eee4e6d374ffa50adc
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2023-04-07 22:04:12 -0600

    Update Assignment2.py

--- hw-clustering/HW/Code/Assignment2.py
+++ hw-clustering/HW/Code/Assignment2.py
@@ -213,8 +213,8 @@ def HPO(hpo_fn, data, labels, seed):
 	# print(label, means, k)
 	# plot(means, stds, k, label=label, runs=runs)
 	# optimizers = [(RF, RF_hps)]
-    # optimizers = [(SVM, SVM_hps)]
-	optimizers = [(MLR, MLR_hps), (HR, HR_hps)]
+    optimizers = [(SVM, SVM_hps)]
+	# optimizers = [(MLR, MLR_hps), (HR, HR_hps)]
 	for optimizer in optimizers:
 		hpo_inst = HPO_Class(hpo_fn=hpo_fn, opt_fn=optimizer[0], hps=optimizer[1], data=data, labels=labels, seed=seed)
 		hpo_inst.optimize()
@@ -295,6 +295,6 @@ if __name__ == "__main__":
 # 05 - 25RF
 # 06 - 100RF
 # 07 - SVM
-# 08 - MLR/HR
+# 09 - MLR/HR
 
 

5dc9da4e00a3bc75ed866335f4325cab2fe481df
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2023-04-07 22:13:43 -0600

    Update Assignment2.py

--- hw-clustering/HW/Code/Assignment2.py
+++ hw-clustering/HW/Code/Assignment2.py
@@ -212,9 +212,9 @@ def plot(mse, label):
 def HPO(hpo_fn, data, labels, seed):
 	# print(label, means, k)
 	# plot(means, stds, k, label=label, runs=runs)
-	optimizers = [(RF, RF_hps)]
+	# optimizers = [(RF, RF_hps)]
 	# optimizers = [(SVM, SVM_hps)]
-	# optimizers = [(MLR, MLR_hps), (HR, HR_hps)]
+	optimizers = [(MLR, MLR_hps), (HR, HR_hps)]
 	for optimizer in optimizers:
 		hpo_inst = HPO_Class(hpo_fn=hpo_fn, opt_fn=optimizer[0], hps=optimizer[1], data=data, labels=labels, seed=seed)
 		hpo_inst.optimize()
@@ -239,6 +239,7 @@ def load_data(fname):
 
 
 def main():
+	global test_data, test_labels
 	# Generate seed
 	# seed = random.randint(0, 1e9)
 	# print("Seed: ", seed)
